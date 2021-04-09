dbb638d68dee351abb55f2ec1bc6081129991014
Patrick Hammarstedt <patrick.hammarstedt@hm.com>
2021-04-09 14:38:19 +0200

    testproject

---
+++ src/libap/Main.java
@@ -0,0 +1,15 @@
+package libap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int[] arr = {1, 5, 7, 8, 5, 6};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 5) {
+            System.out.println("found a five at index " + i);
+            }
+        }
+    }
+}
